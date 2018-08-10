76d5e7c359effe6a3fdd6689d6acd1755475112d
Christian Oder <myself5@carbonrom.org>
2018-08-10 21:16:00 +0200

    Revert "shinano-common: build: Add widevine BLOB include"
    This reverts commit 6ca629fa4ad736a57aee61dad57893ebd8d5ea38.
    
    Change-Id: I6592ee7012bbd75ed287a0524ef3615a8537f385

--- shinano.mk
+++ shinano.mk
@@ -161,4 +161,3 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bc
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/shinano-common/shinano-common-vendor.mk)
-$(call inherit-product, vendor/sony/widevine/widevine.mk)
