1749364cb7e1eec146a8e9bd8465dcaafcb1e63f
arian <arian.kulmer@web.de>
2019-04-07 22:05:15 +0200

    shinano-common: bluetooth: Move vnd_shinano to vnd_generic

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,7 +53,7 @@ BOARD_HAVE_BCM_FM := true
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/shinano-common/bluetooth
-BOARD_CUSTOM_BT_CONFIG := device/sony/shinano-common/bluetooth/vnd_shinano.txt
+BOARD_CUSTOM_BT_CONFIG := device/sony/shinano-common/bluetooth/vnd_generic.txt
 
 # BT/FM (Broadcom): Adjust the sysfs patch for 3.4 kernel
 BOARD_HAVE_BCM_FM_SYSFS := "/sys/bus/platform/drivers/bcm_ldisc/bcm_ldisc/"
