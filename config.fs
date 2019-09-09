4d2822e1302304a8aad476a6defa7b2f77920ed0
Arian <arian.kulmer@web.de>
2019-09-09 16:08:14 +0200

    shinano-common: Create mount points via build system

---
+++ config.fs
@@ -0,0 +1,17 @@
+[idd/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[lta-label/]
+mode: 0555
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[tmp/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
